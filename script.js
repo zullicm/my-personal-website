e3f567224555753622fff22c721293b4de6fad52
Christopher Marzulli <chriszulli15@gmail.com>
2022-04-11 22:39:09 -0400

    added scroll function to one and two

--- script.js
+++ script.js
@@ -13,4 +13,38 @@ function firstMore() {
         firstText.style.display = "inline";
     }
   }
+
+
+  function secondMore() {
+    let secondDots = document.getElementById("secondDots");
+    let secondText = document.getElementById("secondMore");
+    let secondBtn = document.getElementById("secondBtn");
   
+    if (secondDots.style.display === "none") {
+        secondDots.style.display = "inline";
+        secondBtn.innerHTML = "Read more";
+        secondText.style.display = "none";
+    } else {
+        secondDots.style.display = "none";
+        secondBtn.innerHTML = "Read less";
+        secondText.style.display = "inline";
+    }
+  }
+
+
+
+  function thirdMore() {
+    let thirdDots = document.getElementById("thirdDots");
+    let thirdText = document.getElementById("thirdMore");
+    let thirdBtn = document.getElementById("thirdBtn");
+  
+    if (thirdDots.style.display === "none") {
+        thirdDots.style.display = "inline";
+        thirdBtn.innerHTML = "Read more";
+        thirdText.style.display = "none";
+    } else {
+        thirdDots.style.display = "none";
+        thirdBtn.innerHTML = "Read less";
+        thirdText.style.display = "inline";
+    }
+  }
